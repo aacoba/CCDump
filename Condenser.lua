09e86d7b36b9fd38f11261fd10ca79abda1c1c4b
Auke Bakker <aacobaNL@gmail.com>
2012-08-05 14:35:29 +0200

    Fixed very stupid typo :X

--- Condenser.lua
+++ Condenser.lua
@@ -1,5 +1,5 @@
 --one line to give the program's name and an idea of what it does.
---Copyright (C) 1012  Auke Bakker
+--Copyright (C) 2012  Auke Bakker
 --
 --This program is free software; you can redistribute it and/or
 --modify it under the terms of the GNU General Public License
